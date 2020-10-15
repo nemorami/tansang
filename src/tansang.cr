768b63dbf1c1679d890a9ff52abe76666783fa3d
유상선 <nemorami@daum.net>
2020-10-15 18:35:34 +0900

    변경 사항에 대한 커밋 메시지를 입력하십시오. '#' 문자로 시작하는  줄은 무시되고, 메시지를 입력하지 않으면 커밋이 중지됩니다.
     현재 브랜치 master
     브랜치가 'origin/master'에 맞게 업데이트된 상태입니다.
    
     커밋할 변경 사항:
    	새 파일:       .vscode/launch.json
    	새 파일:       .vscode/settings.json
    	새 파일:       .vscode/tasks.json
    	수정함:        src/tansang.cr
    	수정함:        src/token_process.cr
    
     추적하지 않는 파일:
    	test

--- src/tansang.cr
+++ src/tansang.cr
@@ -1,17 +1,23 @@
+require "./token_process"
+
 # TODO: Write documentation for `Tansang`
 module Tansang
-  VERSION = "0.1.0" 
+  VERSION = "0.1.0"
 
   # TODO: Put your code here
 end
 
-io = File.open("./README.md")
+io = File.open("./test")
 
-loop do
-  ch = io.read_byte
-  
-  puts "#{ch}, #{typeof(ch)}"  
- if !ch
-    break
-  end
-end
+tp = TokenProcess.new(io)
+puts tp.nextTkn()
+
+# ch = nil
+# case ch
+# when Nil
+#   puts "ch is nil"
+# when 'A' .. 'Z'
+#   puts "ch is capital"
+
+# end
+
--- src/token_process.cr
+++ src/token_process.cr
@@ -1,54 +1,95 @@
-enum TknKind
-    Lparen  = 40
-    Rparen  = 41
-    Plus  = 43
-    Minus
-    Multi
-    Divi
-    Assign
-    Comma
-    DblQ
-    Equal
-    NotEq
-    Less
-    LessEq
-    Great
-    GreatEq
-    If 
-    Else
-    End
-    Print 
-    Ident 
-    IntNum
-    String 
-    Letter 
-    Digit
-    EofTkn
-    Others
-    END_list
+enum CharKind
+  Lparen     = 40
+  Rparen     = 41
+  Plus       = 43
+  Minus
+  Multi
+  Divi
+  Assign
+  Comma
+  DblQ
+  Equal
+  NotEq
+  Less
+  LessEq
+  Great
+  GreatEq
+  If
+  Else
+  End
+  Print
+  Ident
+  IntNum
+  String
+  Letter
+  Digit
+  EofTkn
+  WhiteSpace
+  Others
+  END_list
+  END
 end
 
 class Token
-    def initialize(@kind : TknKind = Others, @text : String = "", intVal : Int32 = 0)
-    end    
+  def initialize(@kind : CharKind = Others, @text : String = "")
+  end
 end
 
 class TokenProcess
-    def initialize(@fin : File)
+  @ch : Char?
+
+  def initialize(@fin : File)
+  end
+
+  def getCharType : CharKind
+    unless @ch.nil?
+      case @ch
+      when Nil
+        return CharKind::End
+      when '0'..'9'
+        return CharKind::Digit
+      when ('A'..'Z') || ('a'..'z') || '_'
+        return CharKind::Letter
+      else
+        return CharKind::Others
+      end
+      return CharKind::End      
     end
-    def nextTkn()
-        while (ch = @fin.read_char).whitespace?
-            case ctype[ch]
-            when TknKind::Letter
-                while(ctype[ch] == TknKind::Letter || ctype[ch] == TknKind::Digit)
-                    @text += ch
-                    ch = @fin.read_char
-                end
-            when TknKind::Digit
-                num = 0
-                while(ctype[ch] == Digit)
-                    num = num * 10 + ch.to_i
-            end
+  end
+
+  def nextTkn : Token?
+    text = ""
+    loop do
+      @ch = @fin.read_char
+
+      # next if ch.whitespace?
+      ch_type = getCharType
+      case ch_type
+      when CharKind::Letter
+        while (ch_type == CharKind::Letter || ch_type == CharKind::Digit)
+          text += @ch.as(Char)
+          @ch = @fin.read_char
+          ch_type = getCharType
         end
+        puts "text => #{text}"
+        return Token.new(CharKind::Letter, text)
+      when Nil
+        break
+      when CharKind::WhiteSpace # 스페이스면 다음 글자를 가져온다.
+        @ch = @fin.read_char
+      when CharKind::Digit
+        num = 0
+        while (getCharType == CharKind::Digit)
+          num = num * 10 + 0
+          if @ch.nil?
+            0
+          else
+            puts typeof(@ch)
+          end
+          @ch = @fin.read_char
+        end
+        return Token.new(CharKind::Digit, num.to_s)
+      end
     end
+  end
 end
